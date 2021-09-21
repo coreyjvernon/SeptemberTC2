ff56273c7f90343237417e4eabdefae842c70baf
Corey Vernon <coreyjvernon@gmail.com>
2021-09-21 16:55:15 -0400

    initial commit

---
+++ DesktopApp/Script/OrderForm.js
@@ -0,0 +1,25 @@
+﻿function Data()
+{
+  TestedApps.Orders.Run();
+  Project.Variables.ContactInfo.Reset();
+  for(; !Project.Variables.ContactInfo.IsEOF();)
+  {
+    Aliases.Orders.MainForm.MainMenu.Click("Orders|New order...");
+    Aliases.Orders.OrderForm.Group.ProductNames.ClickItem("FamilyAlbum");
+    Aliases.Orders.OrderForm.Group.Quantity.wValue = 10;
+    aqObject.CheckProperty(Aliases.Orders.OrderForm.Group.Discount, "wText", cmpEqual, "15%");
+    Aliases.Orders.OrderForm.Group.Customer.SetText(Project.Variables.ContactInfo.Value("Customer"));
+    Aliases.Orders.OrderForm.Group.Street.SetText(Project.Variables.ContactInfo.Value("Street"));
+    Aliases.Orders.OrderForm.Group.City.SetText(Project.Variables.ContactInfo.Value("City"));
+    Aliases.Orders.OrderForm.Group.State.SetText(Project.Variables.ContactInfo.Value("State"));
+    Aliases.Orders.OrderForm.Group.Zip.SetText(Project.Variables.ContactInfo.Value("Zip Code"));
+    Aliases.Orders.OrderForm.Group.MasterCard.ClickButton();
+    Aliases.Orders.OrderForm.Group.CardNo.SetText("5154785214525412");
+    aqObject.CheckProperty(Aliases.Orders.OrderForm.Group.CardNo, "TextLength", cmpEqual, 16);
+    Aliases.Orders.OrderForm.Group.ExpDate.wDate = Project.Variables.ContactInfo.Value("ExpDate");
+    Aliases.Orders.OrderForm.ButtonOK.ClickButton();
+    Project.Variables.ContactInfo.Next();
+  }
+  Aliases.Orders.MainForm.Close();
+  Aliases.Orders.dlgConfirmation.btnNo.ClickButton();
+}
---
+++ WebApp/Script/ContactScript.js
@@ -0,0 +1,19 @@
+﻿function Contact()
+{
+  Browsers.Item(btChrome).Navigate("https://smartstore.alertsite.com/");
+  Project.Variables.ContactInfo.Reset();
+  for(; !Project.Variables.ContactInfo.IsEOF();)
+  {
+    Aliases.browser.pageShop.header.navUsd.navContactUs.linkContactUs.Click();
+    Aliases.browser.pageContactus.Wait();
+    Aliases.browser.pageContactus.sectionContent.formYourName.textboxYourName.SetText(Project.Variables.ContactInfo.Value("Name"));
+    Aliases.browser.pageContactus.sectionContent.formYourName.textboxYourEmail.SetText(Project.Variables.ContactInfo.Value("Email"));
+    Aliases.browser.pageContactus.sectionContent.formYourName.textareaEnquiry.Keys(Project.Variables.ContactInfo.Value("Inquiry"));
+    Aliases.browser.pageContactus.sectionContent.formYourName.buttonSendEmail.ClickButton();
+    Aliases.browser.pageContactus.Wait();
+    aqObject.CheckProperty(Aliases.browser.pageContactus.sectionContent.panelYourEnquiryHasBeen, "Exists", cmpEqual, true);
+    Project.Variables.ContactInfo.Next();
+  }
+  Aliases.browser.pageContactus.header.link.imageYourStoreName.Click();
+  Aliases.browser.BrowserWindow.Close();
+}
